1b3337bf9506f285dd998958536ce9e039a4e2a1
banelmikhael <banel.mikhael.bm@gmail.com>
2022-10-30 15:51:39 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork (location  = 'go to the office') {
+    location;
+
+    return `This Monday, I will ${location}.`
+}
+
+function wrapAdjective (style = '*') {
+    return function inner (anyWord = 'special') {
+        return `You are ${style}${anyWord}${style}!`
+    }
+}
